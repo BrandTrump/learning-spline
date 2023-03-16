c9c89101b5c82a2059b694daf5a0ad13c711bfb0
Brandon Trump <brandontrump101@gmail.com>
2023-03-16 22:13:28 +1300

    added spline component to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Spline from "@splinetool/react-spline";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="instructions-card">
+        <div className="title">
+          <h2>Move Around!</h2>
+        </div>
+        <div className="keys">
+          <h3>W - Go forward</h3>
+          <h3>S - Go backwards</h3>
+          <h3>A - turn left</h3>
+          <h3>D - turn right</h3>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="spline-container">
+        <Spline scene="https://prod.spline.design/0AkAc1CDzXUadFsY/scene.splinecode" />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
